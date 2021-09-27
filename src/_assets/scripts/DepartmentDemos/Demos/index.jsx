1e925d496319e26beb043e875b3a777cf862b0bd
Jamie Cassidy <jamiec90@gmail.com>
2021-09-27 14:05:14 +0100

    Improve styling of no demos state

--- src/_assets/scripts/DepartmentDemos/Demos/index.jsx
+++ src/_assets/scripts/DepartmentDemos/Demos/index.jsx
@@ -13,7 +13,10 @@ const DigitalDemos = ({ demoDate, demos }) => {
           )}
         </ol>
       :
-        <h3>No demos yet.</h3>
+        <div className="ch-bg--grey-2 ch-rounded ch-clearfix ch-pa--4 sm:ch-mv--10 sm:ch-pa--6 sm:ch-width--7 sm:ch-centered md:ch-width--6 lg:ch-width--5">
+          <h3 className="sm:ch-text--left ch-mb--3 sm:ch-pull--left sm:ch-mt--2 sm:ch-mb--0 sm:ch-fs--4">No demos yet</h3>
+          <a href="/sign-up" className="ch-btn ch-btn--success ch-width--12 sm:ch-width--auto sm:ch-pull--right">Sign up now</a>
+        </div>
       }
     </Fragment>
   )
