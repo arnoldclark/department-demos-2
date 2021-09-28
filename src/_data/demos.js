1b86b416d8843d7d74b248c01b64f3304991fa3f
ACDigitalAdmin <91459548+ACDigitalAdmin@users.noreply.github.com>
2021-09-28 10:39:20 +0100

    Update demos.js
    Change marketing date.

--- src/_data/demos.js
+++ src/_data/demos.js
@@ -1,4 +1,4 @@
 module.exports = {
 	digitalDemoDate: 'Thursday, 30th September 2021 @ 10am',
-	marketingDemoDate: 'Friday, 1st October 2021 @ 10am'
+	marketingDemoDate: 'Friday, 8th October 2021 @ 10am'
 }
