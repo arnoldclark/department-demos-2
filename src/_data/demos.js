0666826c2a009b50074543b588cdbca799b1a70b
ACDigitalAdmin <91459548+ACDigitalAdmin@users.noreply.github.com>
2023-11-22 10:47:41 +0000

    Update demos.js

--- src/_data/demos.js
+++ src/_data/demos.js
@@ -1,4 +1,4 @@
 module.exports = {
-	digitalDemoDate: 'Thursday, 9th November @ 9:30am',
-	marketingDemoDate: 'Wednesday, 15th November @ 10am '
+	digitalDemoDate: 'Wednesday, 20th December @ 9:30am',
+	marketingDemoDate: 'Wednesday, 29th November @ 10am '
 }
